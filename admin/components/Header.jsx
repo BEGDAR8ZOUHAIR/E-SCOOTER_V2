2f00182dac20f841c52ffda0641a39660b950b75
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2023-02-28 14:58:46 +0100

    add header routage

--- admin/components/Header.jsx
+++ admin/components/Header.jsx
@@ -1,12 +1,41 @@
-import React from 'react'
+import { handleClientScriptLoad } from "next/script";
+import React from "react";
 
-const Header = () => {
+
+const Header = () =>
+{
+ 
+  
   return (
-    <div className='flex justify-between px-4 pt-4'>
-        <h2>Dashboard</h2>
-        <h2>Welcome Back, Clint</h2>
-    </div>
-  )
-}
+    <header className="bg-white border-b border-gray-200">
+      <nav className="container mx-auto px-4 py-3 flex justify-between items-center">
+        <div className="flex items-center">
+          <a href="/" className="text-gray-800 text-xl font-bold">
+            Admin
+          </a>
+          <ul className="flex items-center ml-6">
+            <li className="ml-6">
+              <a href="/" className="text-gray-600 hover:text-gray-800">
+                Dashboard
+              </a>
 
-export default Header
+            </li>
+            <li className="ml-6">
+              <a href="/" className="text-gray-600 hover:text-gray-800">
+                Products
+              </a>
+            </li>
+            <li className="ml-6">
+              <a href="/" className="text-gray-600 hover:text-gray-800">
+                Orders
+              </a>
+            </li>
+          </ul>
+        </div>
+        
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
